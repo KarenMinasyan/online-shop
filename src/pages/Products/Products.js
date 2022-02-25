95ebb5ad9dd18a8dae837085dcef8ab928cf6287
KarenMinasyan <kminasyantricking@gmail.com>
2022-02-25 13:12:25 +0400

    add pagination

--- src/pages/Products/Products.js
+++ src/pages/Products/Products.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import Card from '../../components/common/UI/Card/Card';
@@ -6,11 +6,18 @@ import Continer from '../../components/Continer/Continer';
 import { fetchProducts } from '../../redux/ducks/productsDuck';
 import { cartSelector, productsSelector } from '../../helpers/reduxSelectors';
 import styles from './Products.module.css';
+import Pagination from '../../components/common/UI/Pagination/Paginaton';
 
 const Products = () => {
 	const dispatch = useDispatch();
 	const { products } = useSelector(productsSelector);
 	const { cartItems } = useSelector(cartSelector);
+	const [currentPage, setCurrentPage] = useState(1);
+	const [postsPerPage] = useState(8);
+
+	const indexOfLastPost = currentPage * postsPerPage;
+	const indexOfFirstPost = indexOfLastPost - postsPerPage;
+	const currentPosts = products.slice(indexOfFirstPost, indexOfLastPost);
 
 	useEffect(() => {
 		dispatch(fetchProducts());
@@ -20,16 +27,24 @@ const Products = () => {
 		localStorage.setItem('cartItems', JSON.stringify(cartItems));
 	}, [cartItems]);
 
+	const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
 	return (
 		<div className={styles.products}>
 			<Continer>
 				<div className={styles.content}>
-					{products.map((item) => (
+					{currentPosts.map((item) => (
 						<NavLink key={item.id} to={`product/${item.id}`}>
 							<Card props={item} key={item.id} />
 						</NavLink>
 					))}
 				</div>
+				<Pagination
+					postsPerPage={postsPerPage}
+					totalPosts={products.length}
+					currentPage={currentPage}
+					paginate={paginate}
+				/>
 			</Continer>
 		</div>
 	);
