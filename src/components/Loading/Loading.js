d114addd0e1063a6b7e5894add22046660f448c8
KarenMinasyan <kminasyantricking@gmail.com>
2022-02-26 12:39:31 +0400

    add loader and 404 not found

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,5 +1,13 @@
+import styles from './Loading.module.css';
+
 const Loading = () => {
-	return <div>...loading</div>;
+	return (
+		<div className={styles.loaderWrapper}>
+			<svg>
+				<circle cx='70' cy='70' r='50'></circle>
+			</svg>
+		</div>
+	);
 };
 
 export default Loading;
--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,5 +1,18 @@
+import styles from './NotFound.module.css';
+
 const NotFound = () => {
-	return <div>page is not found</div>;
+	return (
+		<div className={styles.flexContainer}>
+			<div className={styles.textCenter}>
+				<h1>
+					<span>4</span>
+					<span>0</span>
+					<span>4</span>
+				</h1>
+				<h3 className={styles.fadeIn}>PAGE NOT FOUND</h3>
+			</div>
+		</div>
+	);
 };
 
 export default NotFound;
